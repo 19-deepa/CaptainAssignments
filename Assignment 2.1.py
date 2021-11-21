b31f2d5fa1de84c2f62ce0553e10b2f1fa6b247b
19-deepa <91127188+19-deepa@users.noreply.github.com>
2021-11-22 00:31:25 +0530

    Add files via upload

---
+++ Assignment 2.1.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+print("Select operation.")
+print("1.add")
+print("2.Subtract")
+while True:
+    choice = input("Enter choice(1/2):")
+    if choice in('1','2'):
+        num1= float(input("Enter first number:"))
+        num2= float(input("Enter second number:"))
+        if choice =='1':
+            print(num1,"+",num2,"=",add(num1,num2))
+        elif choice =='2':
+            print(num1,"-",num2,"=",subtract(num1,num2))
+        break
+    else:
+        print("Invalid input")
+        
